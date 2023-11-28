8637c8725604c6bc594b4e1c96d9ca6deba0cb7a
xzelencikova <takendream@gmail.com>
2023-11-28 09:47:21 +0100

    Load questions

--- src/app/pages/questions-list-page/components/questions-table/questions-table.component.ts
+++ src/app/pages/questions-list-page/components/questions-table/questions-table.component.ts
@@ -2,9 +2,10 @@ import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { ClientService } from 'src/app/services/client.service';
 import {MatPaginator, MatPaginatorModule} from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
-import { ClientModel } from 'src/app/models/client.model';
 import { MatSort } from '@angular/material/sort';
 import { Router } from '@angular/router';
+import { QuestionService } from 'src/app/services/question.service';
+import { QuestionModel } from 'src/app/models/question.model';
 
 
 @Component({
@@ -13,8 +14,8 @@ import { Router } from '@angular/router';
   styleUrls: ['./questions-table.component.css']
 })
 export class QuestionsTableComponent implements OnInit, AfterViewInit {
-  displayedColumns: string[] = ['name', 'surname', 'contract_no', 'registration_date', 'last_phase', 'details'];
-  public dataSource: MatTableDataSource<ClientModel>;
+  displayedColumns: string[] = ['question', 'category', 'actions'];
+  public dataSource: MatTableDataSource<QuestionModel>;
 
   // @ts-ignore
   @ViewChild(MatPaginator) paginator: MatPaginator;
@@ -22,14 +23,15 @@ export class QuestionsTableComponent implements OnInit, AfterViewInit {
   @ViewChild(MatSort) sort: MatSort;
 
 
-  constructor(private clientService: ClientService, private router: Router) {
-    this.dataSource = new MatTableDataSource<ClientModel>();
+  constructor(private questionsService: QuestionService, private router: Router) {
+    this.dataSource = new MatTableDataSource<QuestionModel>();
   }
 
 
   ngOnInit(): void {
-    this.clientService.getClientsList().subscribe(res => {
-      this.dataSource = new MatTableDataSource<ClientModel>(res);
+    this.questionsService.getQuestionsList().subscribe(res => {
+      console.log(res)
+      this.dataSource = new MatTableDataSource<QuestionModel>(res);
       this.dataSource.paginator = this.paginator;
       this.dataSource.sort = this.sort;
     })
@@ -47,8 +49,8 @@ export class QuestionsTableComponent implements OnInit, AfterViewInit {
     }
   }
 
-  showClientOverview(client: ClientModel) {
-    this.router.navigate(["/client-overview", client._id]);
-    this.clientService.selectedClient$.emit(client);
-  }
+  // showClientOverview(client: ClientModel) {
+  //   this.router.navigate(["/client-overview", client._id]);
+  //   this.clientService.selectedClient$.emit(client);
+  // }
 }
