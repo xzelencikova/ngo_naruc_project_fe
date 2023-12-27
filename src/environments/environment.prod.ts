79f957640b866e250bc251a9a6f8ce68685920eb
xzelencikova <takendream@gmail.com>
2023-12-27 11:55:16 +0100

    Update env

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    baseUrl: 'https://ngo-naruc-api.onrender.com'
+    // baseUrl: 'http://localhost:5000'
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: false,
-    baseUrl: 'http://localhost:5000'
+    baseUrl: 'https://ngo-naruc-api.onrender.com'
+    // baseUrl: 'http://localhost:5000'
 }
