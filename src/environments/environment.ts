12c6f6b5eca78d24d24636054278aac8b984bf81
xzelencikova <takendream@gmail.com>
2024-01-24 22:53:19 +0100

    URL to be

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    // baseUrl: 'https://ngo-naruc-api.onrender.com'
-    baseUrl: 'http://localhost:5000'
+    baseUrl: 'https://ngo-naruc-api.onrender.com'
+    // baseUrl: 'http://localhost:5000'
 }
