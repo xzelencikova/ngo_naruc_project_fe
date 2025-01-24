f9b7475845ae23e85eb6e71105fffd900d9c881f
xzelencikova <takendream@gmail.com>
2025-01-24 11:59:06 +0100

    Change year shown in the client table to reflext the year in contract number

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
     // baseUrl: 'https://ngo-naruc-api.onrender.com'
-    baseUrl: 'http://localhost:5000'
+    baseUrl: 'http://localhost:5555/ngo_naruc_project_be/api/v1'
 }
