c4d429b80d9de21304142a5e6b1394aa58991866
SimaNaZemi <simona.zemanova20@gmail.com>
2025-04-15 09:53:00 +0200

    delete client

--- src/app/pages/clients-list-page/components/clients-table/clients-table.component.ts
+++ src/app/pages/clients-list-page/components/clients-table/clients-table.component.ts
@@ -4,7 +4,10 @@ import {MatPaginator, MatPaginatorModule} from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 import { ClientModel } from 'src/app/models/client.model';
 import { MatSort } from '@angular/material/sort';
+import { MatDialog } from '@angular/material/dialog';
 import { Router } from '@angular/router';
+import { AlertService } from 'src/app/components/alert';
+import { DeleteClientWindowComponent } from '../delete-window/delete-window.component';
 
 
 @Component({
@@ -13,7 +16,7 @@ import { Router } from '@angular/router';
   styleUrls: ['./clients-table.component.css']
 })
 export class ClientsTableComponent implements OnInit, AfterViewInit {
-  displayedColumns: string[] = ['name', 'surname', 'contract_no', 'registration_date', 'last_phase', 'details'];
+  displayedColumns: string[] = ['name', 'surname', 'contract_no', 'registration_date', 'last_phase', 'details', 'delete'];
   public dataSource: MatTableDataSource<ClientModel>;
 
   // @ts-ignore
@@ -22,12 +25,25 @@ export class ClientsTableComponent implements OnInit, AfterViewInit {
   @ViewChild(MatSort) sort: MatSort;
 
 
-  constructor(private clientService: ClientService, private router: Router) {
+  constructor(
+    private clientService: ClientService, 
+    private router: Router,
+    private dialog: MatDialog,
+    private alertService: AlertService) {
     this.dataSource = new MatTableDataSource<ClientModel>();
   }
 
 
   ngOnInit(): void {
+    this.clientService.getClientsList().subscribe(res => {
+      this.dataSource = new MatTableDataSource<ClientModel>(res);
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
+    })
+  }
+
+
+  reloadTable() {
     this.clientService.getClientsList().subscribe(res => {
       this.dataSource = new MatTableDataSource<ClientModel>(res);
       this.dataSource.paginator = this.paginator;
@@ -51,4 +67,28 @@ export class ClientsTableComponent implements OnInit, AfterViewInit {
     this.router.navigate(["/client-overview", client._id]);
     this.clientService.selectedClient$.emit(client);
   }
+
+  deleteClientDialog(e: any) {
+    const dialogRef = this.dialog.open(DeleteClientWindowComponent, {
+      data: {
+        client: e
+      }
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        this.clientService.deleteClient(e._id!).subscribe({
+          next: success => {
+            this.alertService.success("Používateľ bol úspešne odstránený.", "Výborne!");
+            this.reloadTable();
+          },
+          error: err => {
+            this.alertService.error("Nepodarilo sa odstrániť používateľa.", "Nastala chyba!")
+          }
+        });
+      }
+    });
+  }
+
+
 }
